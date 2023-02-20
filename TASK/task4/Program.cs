e7f4e656a28eb0d601d2a94d05a2e40280eb05c6
Stepan <sud_pravo@bk.ru>
2023-02-20 20:18:34 +0700

    решили задачу проверяющую 5тизначное число на полиндром

---
+++ TASK/task4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+
+int GetChislo(string messege)
+{
+    Console.Write(messege +" ");
+    return int.Parse(Console.ReadLine());
+}
+
+int value = GetChislo("Введите пятизначное число => ");
+if (value < 10000 || value >99999)
+{
+    Console.WriteLine("Вы ввели не пятизначное число");
+    return;
+}
+bool Met(int value)
+{
+int a = value / 10000;
+int b = (value % 10000) / 1000;
+int c = value % 10 ; 
+int d  = (value  % 100) / 10; 
+if (a == c && d == b )
+{
+  Console.WriteLine("Это число полиндром ");
+  return true;
+}
+Console.WriteLine("Это число не полиндром ");
+return false;
+}
+Met(value);
