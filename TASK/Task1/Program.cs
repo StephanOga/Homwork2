758f1f61258a650914eb756926705b16521d92d1
Stepan <sud_pravo@bk.ru>
2023-02-20 18:59:15 +0700

    решили задачу №10

--- TASK/Task1/Program.cs
+++ TASK/Task1/Program.cs
@@ -1,24 +1,21 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. 
 // 456 -> 5
-//782 -> 8
-//918 -> 1
-//рабиваем на подзадачи:
-// 1. определяем методы: количество 
-// 2.
+// 782 -> 8
+// 918 -> 1
 
-// Метод для введения числа с консоли
-// int GetChislo(string messege)
-//{
-    //Console.Write(messege +" ");
-    //return int.Parse(Console.ReadLine());
-//}
-// int  namber = GetChislo ("Введите трехзначное чило ");
 
-    //if (namber < 100 || namber >=1000)
-    //{
-        //Console.WriteLine("Вы ввели не трех значное чилос, повторите ввод");
-        //return;
-    //} 
-   //Console.WriteLine("Введеное число " + namber);
-   //int secondRank = namber / 10 % 10;
-   //Console.WriteLine("Вторая цифра " + secondRank);
+int GetChislo(string messege)
+{
+    Console.Write(messege +" ");
+    return int.Parse(Console.ReadLine());
+}
+int  namber = GetChislo ("Введите трехзначное чило ");
+
+    if (namber < 100 || namber >=1000)
+    {
+        Console.WriteLine("Вы ввели не трех значное чилос, повторите ввод");
+        return;
+    } 
+Console.WriteLine("Введеное число " + namber);
+int secondRank = namber / 10 % 10;
+Console.WriteLine("Вторая цифра " + secondRank);
